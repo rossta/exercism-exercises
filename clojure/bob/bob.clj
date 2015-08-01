a694d8abadcbdd26a0da66990c228b49369862f0
Ross Kaffenberger <rosskaff@gmail.com>
2015-08-01 18:22:46 -0400

    refactor clojure/bob

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,19 +1,24 @@
 (ns bob)
 
-(defn all-caps? [prompt]
-  (and (re-matches #".*[A-Z].*" prompt)
-       (re-matches #"^[A-Z\d[^\w]]+$" prompt)))
-
-(defn silence? [prompt]
+(defn silent? [prompt]
   (re-matches #"^\s*$" prompt))
 
-(defn ends-with? [prompt c]
-  (= (last prompt) c))
+(defn all-caps? [prompt]
+  (re-matches #"^[A-Z\d[^\w]]+$" prompt))
+
+(defn contains-letter? [prompt]
+  (re-matches #".*[A-Z].*" prompt))
+
+(defn yelling? [prompt]
+  (and (contains-letter? prompt)
+       (all-caps? prompt)))
+
+(defn question? [prompt]
+  (= (last prompt) \?))
 
 (defn response-for [prompt]
   (cond
-    (silence? prompt) "Fine. Be that way!"
-    (all-caps? prompt) "Whoa, chill out!"
-    (ends-with? prompt \!) "Whatever."
-    (ends-with? prompt \?) "Sure."
+    (yelling? prompt) "Whoa, chill out!"
+    (question? prompt) "Sure."
+    (silent? prompt) "Fine. Be that way!"
     :else "Whatever."))
