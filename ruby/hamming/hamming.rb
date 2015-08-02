b83dfccb808ffbea0d2a56a492ff445dbbc963b2
Ross Kaffenberger <rosskaff@gmail.com>
2015-08-02 17:50:55 -0400

    Ruby: Hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,11 @@
+class Hamming
+  VERSION = 1
+
+  def self.compute(left, right)
+    raise ArgumentError unless left.length == right.length
+
+    char_pairs = left.chars.zip(right.chars)
+
+    char_pairs.keep_if { |(a, b)| a != b }.count
+  end
+end
