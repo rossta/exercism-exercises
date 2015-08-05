8f811fd7884b1652029fa07d58d05e449ac2e2d7
Ross Kaffenberger <rosskaff@gmail.com>
2015-08-05 08:42:58 -0400

    Clojure: Anagram refactor from feedback

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,14 +1,14 @@
 (ns anagram
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as string]))
 
 (defn- normalize [text]
-  (str/lower-case text))
+  (string/lower-case text))
 
 (defn- anagrams? [a b]
   (let [c (normalize a)
         d (normalize b)]
-    (and (not (= c d))
-         (= (frequencies c) (frequencies d)))))
+    (and (not= c d)
+         (= (sort c) (sort d)))))
 
 (defn- anagram-for? [a]
   (fn [b] (anagrams? a b)))
