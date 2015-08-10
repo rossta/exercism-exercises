5e656b9e15fb91310f9f83ce1fa307e5197a1180
Ross Kaffenberger <rosskaff@gmail.com>
2015-08-10 09:10:46 -0400

    Clojure: refactor beer song

--- clojure/beer-song/beer_song.clj
+++ clojure/beer-song/beer_song.clj
@@ -2,43 +2,35 @@
   (:require [clojure.string :as str]))
 
 (def go-to-store
-  "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+  "Go to the store and buy some more, 99 bottles of beer on the wall.")
 
-(defn- pluralize-bottles [num]
-  (if (= num 1) "bottle" "bottles"))
-
-(defn- describe-bottles [num]
-  (if (zero? num) "no more" num))
-
-(defn- bottle-phrase [num]
-  (str (describe-bottles num)
-       " "
-       (pluralize-bottles num)))
-
-(defn- next-bottles [num]
-  (bottle-phrase (dec num)))
-
-(defn- current-bottle [num]
-  (if (= num 1) "it" "one"))
+(defn- bottle [num]
+  (condp = num
+    0 "no more bottles"
+    1 "1 bottle"
+    (str num " bottles")))
 
 (defn- take-one-down [num]
   (str "Take "
-       (current-bottle num)
+       (if (= num 1) "it" "one")
        " down and pass it around, "
-       (next-bottles num)
-       " of beer on the wall.\n"))
+       (bottle (dec num))
+       " of beer on the wall."))
 
-(defn- drink-or-buy [num]
+(defn- verse-end [num]
   (if (zero? num)
     go-to-store
     (take-one-down num)))
 
-(defn verse [num]
-  (str (str/capitalize (bottle-phrase num)) " of beer on the wall, "
-       (bottle-phrase num) " of beer.\n"
-       (drink-or-buy num)))
+(defn- verse-start [num]
+  (str (str/capitalize (bottle num)) " of beer on the wall, "
+       (bottle num) " of beer."))
 
-(defn sing [& [start end]]
-  (let [a start
-        b (if (nil? end) 0 end)]
-  (str/join "\n" (map verse (range a (dec b) -1)))))
+(defn verse [num]
+  (str (verse-start num) "\n" (verse-end num) "\n"))
+
+(defn sing
+  ([start end]
+   (str/join "\n" (map verse (range start (dec end) -1))))
+  ([start]
+   (sing start 0)))
