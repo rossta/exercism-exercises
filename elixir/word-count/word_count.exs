ee965e13de97914ff89e6565fff82e3692984145
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-12 12:07:16 -0500

    Update elixir word_count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,22 +7,11 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-    String.downcase(sentence)
-    |> String.replace(~r/\_/, " ")
-    |> String.replace(~r/[^A-Za-zäöü0-9\-\s]/, "")
-    |> String.split(~r/\s+/)
+    String.split(sentence, ~r/[\s\,\_\!&@\$%\^:]/, trim: true)
     |> (Enum.reduce %{}, &Words.count_word/2)
   end
 
   def count_word(word, map) do
-    { get, new_map } = Map.get_and_update(map, word,
-      fn count -> { count, (count || 0) + 1 } end)
-    new_map
-  end
-
-  def count_word(word, map) do
-    { get, new_map } = Map.get_and_update(map, word,
-      fn count -> { count, (count || 0) + 1 } end)
-    new_map
+    Map.update(map, String.downcase(word), 1, &(&1 + 1))
   end
 end
