ad82ee5fab2bc0cae6b09607a217bd3a783cf515
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-12 10:11:06 -0500

    Elxir Bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,22 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      silence?(input) -> "Fine. Be that way!"
+      shouting?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+      true -> "Whatever."
     end
+  end
+
+  defp shouting?(input) do
+    Regex.match?(~r/[[:upper:]]/m, input) && Regex.match?(~r/^[[:upper:]0-9\W]+!?$/m, input)
+  end
+
+  defp silence?(input) do
+    Regex.match?(~r/^\s*$/m, input)
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
   end
 end
