3e0472aeee3c657f582dcd0c1aab8536323c8153
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-13 12:08:09 -0400

    Simplify rle with map_join and better regex

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -8,35 +8,22 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t) :: String.t
   def encode(string) do
-    string
-    |> String.split("", trim: true)
-    |> MyEnum.chunk_while(fn a, b -> a == b end)
-    |> Enum.reduce("", &collapse_chunk/2)
+    Regex.scan(~r/(\w)\1*/, string)
+    |> Enum.map_join(&collapse_chunk/1)
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
     Regex.scan(~r/(\d+)(\w)/, string)
-    |> Enum.map(&expand_chunk/1)
-    |> Enum.join
+    |> Enum.map_join(&expand_chunk/1)
   end
 
-  defp collapse_chunk([], str), do: str
-  defp collapse_chunk([letter | _] = chunk, str) do
-    str <> to_string(Enum.count(chunk)) <> letter
+  defp collapse_chunk([chunk | letter] = match) do
+    "#{String.length(chunk)}#{letter}"
   end
 
-  defp expand_chunk([chunk, count, letter]) do
+  defp expand_chunk([_, count, letter]) do
     {int, _} = Integer.parse(count)
     String.duplicate(letter, int)
-  end
-end
-
-require IEx
-defmodule MyEnum do
-  def chunk_while([], func), do: []
-  def chunk_while([ a | tail ] = list, func) do
-    [Enum.take_while(list, fn b -> func.(a, b) end) |
-     Enum.drop_while(list, fn b -> func.(a, b) end) |> chunk_while(func)]
   end
 end
