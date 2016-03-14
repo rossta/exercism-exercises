4b1c730985c783d693131de5d724d7ce06657ad6
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-14 09:11:56 -0400

    Javascript hello world

--- javascript/hello-world/hello-world.spec.js
+++ javascript/hello-world/hello-world.spec.js
@@ -4,14 +4,14 @@ describe('Hello World', function() {
   var helloWorld = new HelloWorld();
 
   it('says hello world with no name', function() {
-    expect(helloWorld.hello('')).toEqual('Hello, world!');
+    expect(helloWorld.hello('')).toEqual('Hello, World!');
   });
 
-  it('says hello to bob', function() {
+  xit('says hello to bob', function() {
     expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
   });
 
-  it('says hello to sally', function() {
+  xit('says hello to sally', function() {
     expect(helloWorld.hello('Sally')).toEqual('Hello, Sally!');
   });
 });
