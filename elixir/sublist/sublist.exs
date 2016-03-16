cd869184cb0fbafe9915fc12d54b4ab2dd21bff2
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-16 08:10:42 -0400

    Update elixir sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,7 +3,7 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) when a === b, do: :equal
+  def compare(a, a), do: :equal
   def compare(a, b) do
     cond do
       sublist?(a, b) -> :sublist
