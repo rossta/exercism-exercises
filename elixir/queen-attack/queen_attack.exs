df9837ace5196602a4e955590ba2713b0d57c738
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-19 18:51:11 -0400

    Implement elixir queen attack

---
+++ elixir/queen-attack/queen_attack.exs
@@ -0,0 +1,45 @@
+defmodule Queens do
+  @type t :: %Queens{ black: {integer, integer}, white: {integer, integer} }
+  defstruct black: nil, white: nil
+
+  @doc """
+  Creates a new set of Queens
+  """
+  @spec new(nil | list) :: Queens.t()
+  def new do
+    new(black: {7, 3}, white: {0, 3})
+  end
+  def new(white: {row, col}, black: {row, col}), do: raise ArgumentError
+  def new([_|_] = positions) do
+    Map.merge(%Queens{}, Enum.into(positions, %{}))
+  end
+
+  @doc """
+  Gives a string reprentation of the board with
+  white and black queen locations shown
+  """
+  @spec to_string(Queens.t()) :: String.t()
+  def to_string(queens) do
+    List.duplicate("_", 64)
+    |> List.replace_at(linear_position(queens.white), "W")
+    |> List.replace_at(linear_position(queens.black), "B")
+    |> Enum.chunk(8)
+    |> Enum.map(&(Enum.join(&1, " ")))
+    |> Enum.join("\n")
+  end
+
+  defp linear_position({row, column}), do: (row * 8) + column
+
+  @doc """
+  Checks if the queens can attack each other
+  """
+  @spec can_attack?(Queens.t()) :: boolean
+  def can_attack?(%Queens{white: {row, _}, black: {row, _}}), do: true
+  def can_attack?(%Queens{white: {_, col}, black: {_, col}}), do: true
+  def can_attack?(%Queens{white: {row1, col1}, black: {row2, col2}}) do
+    row_diff = row1 - row2
+    col_diff = col1 - col2
+    row_diff == col_diff || row_diff == -col_diff
+  end
+  def can_attack?(%Queens{white: {_, _}, black: {_, _}}), do: false
+end
