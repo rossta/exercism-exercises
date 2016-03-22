7a9d96530fec7a4bf92e5f99b8a55730d7b69289
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-22 17:54:41 -0400

    Use accumulator elixir binary

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -6,12 +6,14 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-    Regex.replace(~r/^.*\p{L}.*$/, string, "")
+    string
     |> String.to_char_list
     |> bit_sum
   end
 
-  defp bit_sum([?0 | tail]), do: bit_sum(tail)
-  defp bit_sum([?1 | tail]), do: :math.pow(2, length(tail)) + bit_sum(tail)
-  defp bit_sum(_), do: 0
+  defp bit_sum(list), do: bit_sum(list, 0)
+  defp bit_sum([], num), do: num
+  defp bit_sum([?0 | tail], num), do: bit_sum(tail, num)
+  defp bit_sum([?1 | tail], num), do: bit_sum(tail, num + :math.pow(2, length(tail)))
+  defp bit_sum(_, _), do: 0
 end
