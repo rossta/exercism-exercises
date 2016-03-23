e029b60392f2087385ca2c0aaba6b884d2502a02
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-22 22:46:13 -0400

    ECMA scriptify food-chain

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,60 +1,60 @@
-var FoodChain = function() {};
+'use strict';
 
-var fly = animalVerse([
+const fly = animalVerse([
   'I know an old lady who swallowed a fly.'
 ], [
   'I don\'t know why she swallowed the fly. Perhaps she\'ll die.'
 ]);
 
-var spider = animalVerse([
+const spider = animalVerse([
   'I know an old lady who swallowed a spider.',
   'It wriggled and jiggled and tickled inside her.'
 ], [
   'She swallowed the spider to catch the fly.'
 ]);
 
-var cat = animalVerse([
+const cat = animalVerse([
   'I know an old lady who swallowed a cat.',
   'Imagine that, to swallow a cat!'
 ], [
   'She swallowed the cat to catch the bird.'
 ]);
 
-var bird = animalVerse([
+const bird = animalVerse([
   'I know an old lady who swallowed a bird.',
   'How absurd to swallow a bird!'
 ], [
   'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.'
 ]);
 
-var dog = animalVerse([
+const dog = animalVerse([
   'I know an old lady who swallowed a dog.',
   'What a hog, to swallow a dog!'
 ], [
   'She swallowed the dog to catch the cat.'
 ]);
 
-var goat = animalVerse([
+const goat = animalVerse([
   'I know an old lady who swallowed a goat.',
   'Just opened her throat and swallowed a goat!'
 ], [
   'She swallowed the goat to catch the dog.',
 ]);
 
-var cow = animalVerse([
+const cow = animalVerse([
   'I know an old lady who swallowed a cow.',
   'I don\'t know how she swallowed a cow!',
 ], [
   'She swallowed the cow to catch the goat.'
 ]);
 
-var horse = animalVerse([
+const horse = animalVerse([
   'I know an old lady who swallowed a horse.'
 ], [
   'She\'s dead, of course!'
 ]);
 
-var CHAIN = [
+const CHAIN = [
   horse,
   cow,
   goat,
@@ -65,20 +65,22 @@ var CHAIN = [
   fly,
 ];
 
-FoodChain.prototype.verses = function(start, finish) {
-  var result = '';
-  for (var i = start; i <= finish; i++) {
-    result += this.verse(i) + '\n';
+class FoodChain {
+  verses(start, finish) {
+    let result = '';
+    for (let i = start; i <= finish; i++) {
+      result += this.verse(i) + '\n';
+    }
+    return result;
   }
-  return result;
-};
 
-FoodChain.prototype.verse = function(num) {
-  return verseParts(num).
-    reduce(function(parts, fn, index) {
-      return fn(parts, index);
-    }, []).join("\n") + '\n';
-};
+  verse(num) {
+    return verseParts(num).
+      reduce((parts, fn, index) => {
+        return fn(parts, index);
+      }, []).join("\n") + '\n';
+  }
+}
 
 function verseParts(num) {
   if (num == CHAIN.length) {
@@ -91,9 +93,9 @@ function verseParts(num) {
 function animalVerse(openings, closings) {
   return function(parts, secondary) {
     if (!secondary) {
-      openings.forEach(function(part) { parts.push(part); });
+      openings.forEach(part => { parts.push(part); });
     }
-    closings.forEach(function(part) { parts.push(part); });
+    closings.forEach(part => { parts.push(part); });
     return parts;
   }
 }
