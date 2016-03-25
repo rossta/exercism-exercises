1dbba1b21ded345fdc1cb1ccd3a38b190de4aa8e
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-25 07:26:34 -0400

    Refactor elixir word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,11 +6,14 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
-    String.split(sentence, ~r/[\s\,\_\!&@\$%\^:]/, trim: true)
-    |> (Enum.reduce %{}, &Words.count_word/2)
+    sentence
+    |> String.split(~r/[\s\,\_\!&@\$%\^:]/, trim: true)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reduce(%{}, &Words.count_word/2)
   end
 
   def count_word(word, map) do
-    Map.update(map, String.downcase(word), 1, &(&1 + 1))
+    map
+    |> Map.update(word, 1, &(&1 + 1))
   end
 end
