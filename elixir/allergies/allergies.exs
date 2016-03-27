d8d2c86f0a1c212cfe5d19ae67d8291f415d8377
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-27 08:38:55 -0400

    Use Bitwise for elixir allergies implementation

--- elixir/allergies/allergies.exs
+++ elixir/allergies/allergies.exs
@@ -2,21 +2,19 @@ defmodule Allergies do
   @allergens ~w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats]
   @max round :math.pow(2, length(@allergens))
 
+  use Bitwise
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t]
   def list(flags) when flags > @max, do: list(flags - @max)
   def list(flags) do
-    flags
-    |> do_list([])
-  end
-
-  defp do_list(0, allergies), do: allergies
-  defp do_list(flags, allergies) do
-    power = round Float.floor(:math.log2(flags))
-    product = round :math.pow(2, power)
-    do_list(flags - product, [Enum.at(@allergens, power) | allergies])
+    @allergens
+    |> Enum.with_index
+    |> Enum.map(fn({key, index}) -> {key, round(:math.pow(2, index))} end)
+    |> Enum.filter(fn({key, power}) -> (flags &&& power) === power end)
+    |> Keyword.keys
   end
 
   @doc """
