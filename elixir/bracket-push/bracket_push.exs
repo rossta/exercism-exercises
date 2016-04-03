2c5c7fe7777f88c596723cabd29379c65f3ac35f
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-03 15:01:44 -0400

    Simplify pattern matching on elixir bracket push

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -5,23 +5,16 @@ defmodule BracketPush do
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
     str
+    |> String.replace(~r/[^\(\)\{\}\[\]]/, "")
     |> String.graphemes
     |> do_check([])
   end
 
   def do_check([], []), do: true
   def do_check([], _), do: false
-  def do_check(["[" | tail], stack), do: do_check(tail, ["[" | stack])
-  def do_check(["]" | tail], ["[" | stack]), do: do_check(tail, stack)
-  def do_check(["]" | _], _), do: false
-
-  def do_check(["{" | tail], stack), do: do_check(tail, ["{" | stack])
-  def do_check(["}" | tail], ["{" | stack]), do: do_check(tail, stack)
-  def do_check(["}" | _], _), do: false
-
-  def do_check(["(" | tail], stack), do: do_check(tail, ["(" | stack])
-  def do_check([")" | tail], ["(" | stack]), do: do_check(tail, stack)
-  def do_check([")" | _], _), do: false
-
-  def do_check([_ | tail], stack), do: do_check(tail, stack)
+  def do_check(["[" | tail], stack), do: do_check(tail, ["]" | stack])
+  def do_check(["{" | tail], stack), do: do_check(tail, ["}" | stack])
+  def do_check(["(" | tail], stack), do: do_check(tail, [")" | stack])
+  def do_check([chr | tail], [chr | stack]), do: do_check(tail, stack)
+  def do_check(_, _), do: false
 end
