0d1975c8f7199f1cdaf43f5a1996687eaeddbca9
Ross Kaffenberger <rosskaff@gmail.com>
2016-09-30 08:36:16 -0400

    WIP elixir forth

---
+++ elixir/forth/forth.exs
@@ -0,0 +1,54 @@
+defmodule Forth do
+  @opaque evaluator :: any
+
+  @doc """
+  Create a new evaluator.
+  """
+  @spec new() :: evaluator
+  def new() do
+    []
+  end
+
+  @doc """
+  Evaluate an input string, updating the evaluator state.
+  """
+  @spec eval(evaluator, String.t) :: evaluator
+  def eval(ev, s) do
+    s |> normalize_input
+  end
+
+  defp normalize_input(s) do
+    s
+    |> String.graphemes
+    |> Enum.filter(&(String.match?(&1, ~r/[\A-\z\d]+/)))
+  end
+
+  @doc """
+  Return the current stack as a string with the element on top of the stack
+  being the rightmost element in the string.
+  """
+  @spec format_stack(evaluator) :: String.t
+  def format_stack(ev) do
+    ev |> Enum.join(" ")
+  end
+
+  defmodule StackUnderflow do
+    defexception []
+    def exception(_), do: "stack underflow"
+  end
+
+  defmodule InvalidWord do
+    defexception [:word]
+    def exception(e), do: "invalid word: #{inspect e.word}"
+  end
+
+  defmodule UnknownWord do
+    defexception [:word]
+    def exception(e), do: "unknown word: #{inspect e.word}"
+  end
+
+  defmodule DivisionByZero do
+    defexception []
+    def exception(_), do: "division by zero"
+  end
+end
