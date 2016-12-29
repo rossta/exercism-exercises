9f1f434b8a420275aec5f6953803db21e1d46dd2
Ross Kaffenberger <rosskaff@gmail.com>
2016-12-29 12:38:45 -0500

    Update ruby nth prime

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -2,21 +2,21 @@ module Prime
   module_function
 
   def nth(number)
-    raise ArgumentError, "Number must be greater than zero" if number < 1
-    return 2 if number < 2
+    raise ArgumentError, "Argument must be greater than zero but was #{number}" if number < 1
+    return 2 if number == 1
 
-    current = 1
-    candidate = 2
-    while current < number
-      candidate += 1
-      current += 1 if prime?(candidate)
+    count = 1
+    candidate = 1
+    while count < number
+      candidate += 2
+      count += 1 if prime?(candidate)
     end
     candidate
   end
 
   def prime?(number)
-    !!2.upto(Math.sqrt(number).floor).each do |i|
-      return false if number % i == 0
+    3.upto(Math.sqrt(number).floor).none? do |i|
+      number % i == 0
     end
   end
 end
