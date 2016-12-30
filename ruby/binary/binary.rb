a0f147fba9346d9263269c6744f0bc306ef9f54d
Ross Kaffenberger <rosskaff@gmail.com>
2016-12-30 09:19:46 -0500

    Update ruby binary

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,14 +1,12 @@
 class Binary
   def self.to_decimal(input)
     raise ArgumentError, "Input must contain only 0 and 1" unless input.match(/^[01]+$/)
-    return input.to_i if input.length == 1
 
     input
       .split('')
       .reverse
       .each_with_index
-      .select { |(digit, _factor)| digit == '1' }
-      .map { |(_digit, factor)| 2**factor }
+      .map { |digit, factor| digit.to_i * 2**factor }
       .reduce(&:+)
   end
 end
