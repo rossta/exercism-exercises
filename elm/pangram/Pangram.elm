a04417a8c02ecf5a422dcb44299565c2f990b29d
Ross Kaffenberger <rosskaff@gmail.com>
2017-01-07 17:46:38 -0500

    Update elm pangram

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -4,25 +4,16 @@ import Set
 
 
 alphabet =
-    Set.fromList [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+    "abcdefghijklmnopqrstuvwxyz"
+        |> String.toList
+        |> Set.fromList
 
 
 isPangram : String -> Bool
 isPangram text =
     text
         |> String.toLower
-        |> String.split ""
+        |> String.toList
         |> Set.fromList
-        |> Set.toList
-        |> List.filter inAlphabet
-        |> matchesAlphabet
-
-
-inAlphabet : String -> Bool
-inAlphabet char =
-    Set.member char alphabet
-
-
-matchesAlphabet : List String -> Bool
-matchesAlphabet chars =
-    Set.size alphabet == List.length chars
+        |> Set.intersect alphabet
+        |> (==) alphabet
