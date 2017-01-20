6d276b644d390933dc6d655acb86e8865b056dc5
Ross Kaffenberger <rosskaff@gmail.com>
2017-01-19 20:55:49 -0500

    Update elm Triangle

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -15,17 +15,37 @@ type Triangle
 triangleKind : Float -> Float -> Float -> Result String Triangle
 triangleKind a b c =
     case List.sort [ a, b, c ] of
-        x :: y :: z :: [] ->
-            if x <= 0 || y <= 0 || z <= 0 then
+        x :: y :: z :: _ ->
+            if violatesLength x y z then
                 Err "Invalid lengths"
-            else if x + y <= z then
+            else if violatesInequality x y z then
                 Err "Violates inequality"
-            else if x == y && y == z then
+            else if isEquilateral x y z then
                 Ok Equilateral
-            else if x == y || y == z then
+            else if isIsoceles x y z then
                 Ok Isosceles
             else
                 Ok Scalene
 
         _ ->
             Ok Scalene
+
+
+isEquilateral : Float -> Float -> Float -> Bool
+isEquilateral a b c =
+    a == b && b == c
+
+
+isIsoceles : Float -> Float -> Float -> Bool
+isIsoceles a b c =
+    a == b || b == c
+
+
+violatesLength : Float -> Float -> Float -> Bool
+violatesLength a b c =
+    a <= 0 || b <= 0 || c <= 0
+
+
+violatesInequality : Float -> Float -> Float -> Bool
+violatesInequality a b c =
+    a + b <= c
